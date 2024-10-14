7b9bac0f0d00e5c2cb9d8f386976c5bff63704c9
Adi <adiabbeve16@gmail.com>
2024-10-14 18:58:35 +0300

    removing soil and buttons

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const dirtCells = document.querySelectorAll('.dirt-cell');
+const soilCells = document.querySelectorAll('.soil-cell');
+const shovel = document.getElementById('shovel')
 
-dirtCells.forEach(cell => {
-  cell.addEventListener('click', () => {
+  shovel.addEventListener('click',function(){
+  soilCells.forEach(cell => { cell.addEventListener('click', () => {
     cell.style.backgroundImage = 'none';
   });
 });
+})
+
+const stoneCells = document.querySelectorAll('.stone-cell');
+const pickaxe = document.getElementById('pickaxe');
+pickaxe.addEventListener('click',function(){
+  stoneCells.forEach(cell => { cell.addEventListener('click', () => {
+    cell.style.backgroundImage = 'none';
+  });
+});
+})
+
+
+        
